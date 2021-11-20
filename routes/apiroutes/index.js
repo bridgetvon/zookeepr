4b97bddd7194c2af91ff2d0871fa5bff51759044
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-19 22:02:01 -0700

    zookeper tests and routes

--- routes/apiroutes/index.js
+++ routes/apiroutes/index.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 const animalRoutes = require('../apiroutes/animalRoutes');
 
 router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
 
 module.exports = router;
